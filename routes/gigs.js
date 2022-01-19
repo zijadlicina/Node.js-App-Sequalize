60d8f76be7216c06598245a84ea54e4ed47eff20
userName1 <hello@gmail.com>
2022-01-19 18:50:14 +0100

    Create method, insert into DB

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,13 +3,36 @@ const router = express.Router();
 const { db } = require('../config/database.js');
 const Gig = require('../models/Gig');
 
+// Get gig list
 router.get('/', (req, res) => {
-    // Gig - any method that is available with seq
-    Gig.findAll()   // 'findAll' return a promise
+    Gig.findAll()
     .then(gigs => {
         console.log(gigs)
         res.sendStatus(200);
     })
+    .catch(err => console.log(err))
+})
+
+// Add a gig -- this is only because we want add the date to database
+router.get('/add', (req, res) => {
+    // This data ultimately come from, but now we hard-code that
+    const data = {
+        title: "Looking for a React developer",
+        technologies: "react, javascript, html, css",
+        budget: '$3000',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus lorem, tincidunt sagittis consequat quis, feugiat vitae ligula. Mauris aliquam arcu nec fermentum maximus. Quisque ligula lorem, dapibus at mi sit amet, pulvinar ornare mauris. Donec id purus odio. Integer egestas tristique nulla, consequat consectetur nunc ullamcorper ac. Maecenas quis ligula eu neque molestie consectetur et quis elit.',
+        contant_mail: 'user1@gmail.com'
+    }
+    let { title, technologies, budget, description, contant_mail} = data;
+    // Insert into table
+    Gig.create({
+        title,
+        technologies,
+        description,
+        budget,
+        contant_mail
+    })
+    .then(Gig => res.redirect('/gigs'))
     .catch(err => console.log(err))
 })
 
